b795f134292854cfbd26ece19754a0bec62e1652
Rômulo Evangelista Ferreira <romuloevangelista2011@gmail.com>
2025-05-28 13:49:16 -0300

    chore: add config to test app

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  env: {
+    test: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript',
+      ],
+    },
+  },
 };

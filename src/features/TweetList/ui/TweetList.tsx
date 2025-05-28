1ae02ee778b029aa4e51178de2f4d75b15f50b19
Rômulo Evangelista Ferreira <romuloevangelista2011@gmail.com>
2025-05-28 18:00:21 -0300

    feat(tweet-list): infinite scroll and ui improvements

--- src/features/TweetList/ui/TweetList.tsx
+++ src/features/TweetList/ui/TweetList.tsx
@@ -1,49 +1,72 @@
-import React, {ReactElement, useEffect} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {ReactElement, useEffect, useMemo, useRef, useState} from 'react';
+import {ActivityIndicator, StyleSheet, View, ViewStyle} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
-import {connect} from 'react-redux';
 
-import {BasicStyle, ITweet, RootState} from './../../../types';
+import {BasicStyle, ITweet} from './../../../types';
 import {Tweet} from './../../../features/Tweet/ui/Tweet';
-import {useAppDispatch} from './../../../hooks';
-import {fetchUserTweets} from './../../../features/TweetList/state/tweets.thunk';
+import {useAppDispatch, useAppSelector} from './../../../hooks';
+import {fetchTweets} from './../../../features/TweetList/state/tweets.thunk';
 
-interface ITweetListProps {
-  tweets: Array<ITweet>;
-}
-
-function TweetListComponent({tweets}: ITweetListProps): ReactElement {
+export function TweetList(): ReactElement {
+  const tweets = useAppSelector(state => state.tweets.data).filter(
+    tweet => tweet.sender,
+  );
   const dispatch = useAppDispatch();
+  const debounceRef = useRef<NodeJS.Timeout | null>(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [nextData, setNextData] = useState<ITweet[]>([]);
+  const data: ITweet[] = useMemo(() => {
+    if (!nextData.length) {
+      return tweets.slice(0, 5);
+    }
+    return [...nextData];
+  }, [nextData, tweets]);
+
+  const onEndReached = async () => {
+    setIsLoading(true);
+
+    debounceRef.current = setTimeout(() => {
+      setNextData([...data, ...tweets.slice(data.length, data.length + 5)]);
+      setIsLoading(false);
+      debounceRef.current = null; // clean it to next call
+    }, 1500);
+  };
 
   useEffect(() => {
-    dispatch(fetchUserTweets('jsmith'));
+    dispatch(fetchTweets());
   }, [dispatch]);
 
   return (
     <View style={styles.container}>
       <FlatList
-        data={tweets}
+        data={data}
+        onEndReached={onEndReached}
         renderItem={tweet => <Tweet tweet={tweet.item} />}
+        // eslint-disable-next-line react/no-unstable-nested-components
+        ListFooterComponent={() =>
+          isLoading && <ActivityIndicator style={styles.loading} size="large" />
+        }
       />
     </View>
   );
 }
 
-const mapStateToProps = (state: RootState) =>
-  ({
-    tweets: state.tweets.data,
-  } as ITweetListProps);
+interface AdditionalStyle {
+  loading: ViewStyle;
+}
 
-export const TweetList = connect(mapStateToProps)(TweetListComponent);
-
-const styles: Partial<BasicStyle> = StyleSheet.create<Partial<BasicStyle>>({
+const styles: Partial<BasicStyle> & AdditionalStyle = StyleSheet.create<
+  Partial<BasicStyle> & AdditionalStyle
+>({
   container: {
     backgroundColor: 'white',
     flex: 1,
     paddingBottom: 8,
     paddingRight: 8,
     paddingLeft: 8,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
+  },
+  loading: {
+    padding: 20,
   },
 });
